f6507c7a56344495f617fc0fe9b2b8aae6573dd4
crystalline02 <875870639@qq.com>
2024-02-15 19:43:18 +0800

    清空仓库

--- bin/shaders/comile.bat
+++
@@ -1,3 +0,0 @@
-D:/VulkanSDK/1.3.268.0/Bin/glslc.exe triangle.vert -o triangle_vert.spv
-D:/VulkanSDK/1.3.268.0/Bin/glslc.exe triangle.frag -o triangle_frag.spv
-pause
--- build/3rdParty/glfw/src/CMakeFiles/glfw.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "C:/Users/ZhenKou/OneDrive/project/vulkan_learn/lib/libglfw3.a"
-)
